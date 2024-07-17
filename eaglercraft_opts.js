066575addbae8488d88cfa5dec492ff4eca0a08c
CoolHerobrineYT <businesscoolherobrine@gmail.com>
2024-07-17 17:18:52 +0200

    Update eaglercraft_opts.js

--- eaglercraft_opts.js
+++ eaglercraft_opts.js
@@ -1,9 +1,9 @@
-window.eaglercraftXOpts = () => ({
+window.eaglercraftXpts = () => ({
 	lang: "en_US",
   // joinServer: "ws" + window.location.href.slice(4),
   // assetsURI: "custom.epk",
 	servers: [
-		{ addr: "ws" + window.location.href.slice(4), name: "EaglercraftX Server" }
+		{ addr: "ws" + window.location.href.slice(4), name: "Zentic.cc" }
 	]
 });
 // document.title = "zentic.cc:tm:";
